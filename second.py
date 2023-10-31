ee6e7fddf7d2319b2c7ebb0e89a227527a8feb19
spnermac <147335129+spnermac@users.noreply.github.com>
2023-10-31 14:45:15 +0300

    Update second.py

--- second.py
+++ second.py
@@ -2,7 +2,7 @@
 
 participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
-def find_common_participants(str1,str2, sign):
+def find_common_participants(str1,str2, sign=','):
     str1 = set(str1.split(sign))
     str2 = str2.split(sign)
     common = str1.intersection(str2)
